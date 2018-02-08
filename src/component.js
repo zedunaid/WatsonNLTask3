168760a3d2ae58af8bd3c1b2452668e4e966058d
ZUNAID SORATHIYA <zunaidsorathiya9@gmail.com>
2018-02-08 12:21:26 +0530

    JSON parse #2

--- src/component.js
+++ src/component.js
@@ -4,36 +4,53 @@ import axios from 'axios';
 /** Parent to child communication, we use prop. State- component internal record keeping (Update some amount
  *  of data over time) */
 export default class OutputScreen extends React.Component {
-  state={watdata:[]};
+  constructor(props) {
+    super(props);
+    this.renderCategory = this.renderCategory.bind(this);
+    this.state={watdata:[]};
+}
+
   componentWillMount()
   {
     const {state} = this.props.navigation;
     
-  axios.post('https://app.accumulate65.hasura-app.io/api',
+  axios.post('https://app.accumulate65.hasura-app.io/api2',
   {
      username:'Zedunaid',
     type:state.params.inp,
     input:state.params.statement
    })
-  .then(response =>this.setState({watdata:response.data}))
-  .catch(error => {
-    console.log(error.response)
+ .then(response =>this.setState({watdata:Object.values(response.data)})) 
+ .catch(error => {
+  console.log(error.response)
 });
-  }
 
+  }
   renderCategory()
   {
-    return this.state.watdata.map((data) => (<Text>{data.categories[0].label}</Text>));
+  /* return this.state.watdata.map((data) => data.categories.map((category)=>(<Text>{category[1].label}</Text>)));*/
+   /*return this.state.watdata.map((data)=>data.Object.categories.map((category)=>(<Text>{category[1].label}</Text>)));*/
+   
+   return this.state.watdata.map((data)=>(<View style={{marginLeft:5}}>
+   <Text>{String(data[0].label).substring(String(data[0].label).lastIndexOf("/") + 1, String(data[0].label).length)}                                {parseFloat(String(data[0].score)).toFixed(4)*100} %</Text>
+   <Text>{String(data[1].label).substring(String(data[1].label).lastIndexOf("/") + 1, String(data[1].label).length)}                                {parseFloat(String(data[1].score)).toFixed(4)*100} %</Text>
+   <Text>{String(data[2].label).substring(String(data[2].label).lastIndexOf("/") + 1, String(data[2].label).length)}                                {parseFloat(String(data[2].score)).toFixed(4)*100} %</Text>
+   </View>));
+      
+  
   }
+ 
  
   render() {
     console.log(this.state);
-    
+    if(!this.state.watdata){
+      return null;
+  }
     return (
       <View>
-         <Text>Your Category: </Text>
-        <Text>Name                                    Score</Text>
-        <Text>{this.renderCategory()}</Text>
+         <Text style={{fontWeight:"bold",marginLeft:5}}>Your Results: </Text>
+        <Text style={{fontWeight:"bold",marginLeft:5}}>Category Name                Confidence</Text>
+        <View>{this.renderCategory()}</View>
       </View>
     );
   }
